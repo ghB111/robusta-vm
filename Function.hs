d407b9872100dc1dd9899d9e761ec4a32c4618e3
Roman <b111forjob@gmail.com>
2022-11-16 22:56:25 +0700

    stuff

--- Function.hs
+++ Function.hs
@@ -1,16 +1,28 @@
 module Function ( Function(..) )
 where
 
-import Types ( Type(..) )
+import Control.Monad.State.Lazy
+
+import Types ( Value(..), Type(..) )
+
+data FunctionState
+    = FunctionState { variables :: [Value] }
+type Variables = [Value]
+
+performInstruction :: State FunctionState FunctionState
+performInstruction
+
+-- instance Functor FunctionState where
+--     fmap = undefined
 
 data Instruction = Instruction
+    deriving (Show, Read)
 
 data Function = Function { name :: String
                          , argTypes :: [Type]
                          , returnType :: Type
                          , instructions :: [Instruction]
-                         }
-                         deriving (Show, Read)
+                         } deriving (Show, Read)
 
 exFunction = Function "examples.ex" [IntT] VoidT []
 
--- Types.hs
+++ Types.hs
@@ -1,12 +1,36 @@
-module Types ( Type (..) )
+{-# LANGUAGE FlexibleInstances #-}
+module Types ( Value(..), Type(..) )
 where
 
--- things that can be on stack
-data Type = VoidT | StringT | IntT -- | ObjRefT
+data Value 
+    = VoidV ()
+    | IntV Int
+    | StringV String
+    deriving (Show, Read)
 
-class VmValue a
-    getType
+data Type
+    = VoidT
+    | IntT
+    | StringT
+    deriving (Show, Read)
 
-class VmTypeConvertible a
-    toVmType :: a -> Type
+vType :: Value -> Type
+vType (VoidV _) = VoidT
+vType (IntV _) = IntT
+vType (StringV _) = StringT
+
+class VmValue a where
+    wrap :: a -> Value
+
+instance VmValue Int where
+    wrap x = IntV x
+
+instance VmValue ([] Char) where
+    wrap x = StringV x
+
+instance VmValue () where
+    wrap x = VoidV x
+
+-- -- class VmTypeConvertible a
+-- --     toVmType :: a -> Type
 
