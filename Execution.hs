9a49a4a984776234b43601c419b77a8af784de57
Roman <b111forjob@gmail.com>
2022-11-18 19:58:38 +0700

    ws

--- Execution.hs
+++ Execution.hs
@@ -21,36 +21,44 @@ performInstruction Nop  = return ()
 performInstruction (Goto to) = do
     frame <- get
     put (frame {pc = to})
+
 performInstruction IAdd = do
     frame <- get
     let ((IntV x1):(IntV x2):xs) = stack frame
     let r = x1 + x2
     put $ frame { stack = (wrap r):xs }
+
 performInstruction IMul = do
     frame <- get
     let ((IntV x1):(IntV x2):xs) = stack frame
     let r = x1 * x2
     put $ frame { stack = (wrap r):xs }
+
 performInstruction ISub = do
     frame <- get
     let ((IntV x1):(IntV x2):xs) = stack frame
     let r = x2 - x1
     put $ frame { stack = (wrap r):xs }
+
 performInstruction INeg = do
     frame <- get
     let ((IntV x):xs) = stack frame
     put $ frame { stack = wrap (-x):xs }
+
 performInstruction IConst0 = do
     frame@Frame{stack} <- get
     put $ frame { stack = wrap (0 :: Int) : stack }
+
 performInstruction IConst1 = do
     frame@Frame{stack} <- get
     put $ frame { stack = wrap (1 :: Int) : stack }
+
 performInstruction IDiv = do
     frame@Frame{stack} <- get
     let ( (IntV x1) : (IntV x2) : xs ) = stack
     let r = x2 `mod` x1
     put $ frame { stack = wrap r : xs }
+
     -- | IfICmpEq { gotoDest :: Int }
     -- | IfICmpGe { gotoDest :: Int }
     -- | IfICmpGt { gotoDest :: Int }
